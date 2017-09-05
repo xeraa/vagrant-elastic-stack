34866dcfe0e939fca46e561ebdb7a384d2c38722
Philipp Krenn <pk@xeraa.net>
2017-09-05 11:21:42 +0200

    CHANGE to 6.0.0-beta2; authentication is still broken

--- elastic-stack/all.sh
+++ elastic-stack/all.sh
@@ -4,6 +4,7 @@
 ansible-playbook /elastic-stack/1_configure-elasticsearch.yml
 ansible-playbook /elastic-stack/2_configure-kibana.yml
 ansible-playbook /elastic-stack/3_configure-logstash.yml
+ansible-playbook /elastic-stack/4_configure-auditbeat.yml
 ansible-playbook /elastic-stack/4_configure-filebeat.yml
 ansible-playbook /elastic-stack/4_configure-heartbeat.yml
 ansible-playbook /elastic-stack/4_configure-metricbeat.yml
