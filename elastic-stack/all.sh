84f77751ec0694a7a1ad9a13d5a5ef9b24d27441
David Pilato <david@pilato.fr>
2018-06-19 16:26:58 +0200

    Remove manual configuration of dashboards

--- elastic-stack/all.sh
+++ elastic-stack/all.sh
@@ -9,4 +9,3 @@ ansible-playbook /elastic-stack/4_configure-filebeat.yml
 ansible-playbook /elastic-stack/4_configure-heartbeat.yml
 ansible-playbook /elastic-stack/4_configure-metricbeat.yml
 ansible-playbook /elastic-stack/4_configure-packetbeat.yml
-ansible-playbook /elastic-stack/5_configure-dashboards.yml
